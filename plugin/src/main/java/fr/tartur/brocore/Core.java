e3a2f35349569a4b2f0ddce6e62c2f8017ef7c50
tarturr <tartur.dev@gmail.com>
2025-05-22 22:00:12 +0200

    Reorganized code related to "/bro" command creation.

--- plugin/src/main/java/fr/tartur/brocore/Core.java
+++ plugin/src/main/java/fr/tartur/brocore/Core.java
@@ -1,13 +1,9 @@
 package fr.tartur.brocore;
 
-import com.mojang.brigadier.arguments.DoubleArgumentType;
-import com.mojang.brigadier.arguments.StringArgumentType;
 import fr.tartur.brocore.commands.BroDataCommand;
 import fr.tartur.brocore.entity.BroPlayerManager;
 import fr.tartur.brocore.entity.BroPlayerManagerImpl;
 import fr.tartur.brocore.events.PlayerInOutEvent;
-import io.papermc.paper.command.brigadier.Commands;
-import io.papermc.paper.command.brigadier.argument.ArgumentTypes;
 import io.papermc.paper.plugin.lifecycle.event.types.LifecycleEvents;
 import org.bukkit.plugin.java.JavaPlugin;
 
@@ -27,27 +23,9 @@ public class Core extends JavaPlugin {
         
         getServer().getPluginManager().registerEvents(new PlayerInOutEvent(this.manager), this);
         
-        this.getLifecycleManager().registerEventHandler(LifecycleEvents.COMMANDS, commands -> {
-            final BroDataCommand data = new BroDataCommand(this.manager);
-            final var field = Commands.argument("field", StringArgumentType.word())
-                    .suggests(data::suggestField);
-            
-            // "/bro <player> set  <field> <amount>"
-            // "/bro <player> info <field>"
-            commands.registrar().register(Commands.literal("bro")
-                    .then(Commands.argument("player", ArgumentTypes.player())
-                            .then(Commands.literal("set")
-                                    .then(field
-                                            .then(Commands.argument("amount", DoubleArgumentType.doubleArg(0d))
-                                                    .executes(data::setCommand)
-                                            )
-                                    )
-                            )
-                            .then(Commands.literal("info")
-                                    .then(field.executes(data::infoCommand)))
-                    )
-                    .build());
-        });
+        this.getLifecycleManager().registerEventHandler(LifecycleEvents.COMMANDS, commands ->
+            commands.registrar().register(new BroDataCommand(this.manager).getCommand())
+        );
         
         getLogger().info("The BROS CORE is LOADED!!! GLHF my BABYCHOUUUUS");
     }
--- plugin/src/main/java/fr/tartur/brocore/commands/BroDataCommand.java
+++ plugin/src/main/java/fr/tartur/brocore/commands/BroDataCommand.java
@@ -1,13 +1,18 @@
 package fr.tartur.brocore.commands;
 
 import com.mojang.brigadier.Command;
+import com.mojang.brigadier.arguments.DoubleArgumentType;
+import com.mojang.brigadier.arguments.StringArgumentType;
 import com.mojang.brigadier.context.CommandContext;
 import com.mojang.brigadier.exceptions.CommandSyntaxException;
 import com.mojang.brigadier.suggestion.Suggestions;
 import com.mojang.brigadier.suggestion.SuggestionsBuilder;
+import com.mojang.brigadier.tree.LiteralCommandNode;
 import fr.tartur.brocore.entity.BroPlayer;
 import fr.tartur.brocore.entity.BroPlayerManager;
 import io.papermc.paper.command.brigadier.CommandSourceStack;
+import io.papermc.paper.command.brigadier.Commands;
+import io.papermc.paper.command.brigadier.argument.ArgumentTypes;
 import io.papermc.paper.command.brigadier.argument.resolvers.selector.PlayerSelectorArgumentResolver;
 import net.kyori.adventure.text.Component;
 import net.kyori.adventure.text.format.NamedTextColor;
@@ -22,9 +27,28 @@ public class BroDataCommand {
     private static final List<String> FIELDS = List.of("experience", "balance");
     
     private final BroPlayerManager manager;
+    private final LiteralCommandNode<CommandSourceStack> command;
 
+    // "/bro <player> set  <field> <amount>"
+    // "/bro <player> info <field>"
     public BroDataCommand(BroPlayerManager manager) {
         this.manager = manager;
+
+        final var field = Commands.argument("field", StringArgumentType.word())
+                .suggests(this::suggestField);
+        this.command = Commands.literal("bro")
+                .then(Commands.argument("player", ArgumentTypes.player())
+                        .then(Commands.literal("set")
+                                .then(field
+                                        .then(Commands.argument("amount", DoubleArgumentType.doubleArg(0d))
+                                                .executes(this::setCommand)
+                                        )
+                                )
+                        )
+                        .then(Commands.literal("info")
+                                .then(field.executes(this::infoCommand)))
+                )
+                .build();
     }
 
     public CompletableFuture<Suggestions> suggestField(CommandContext<CommandSourceStack> ctx, SuggestionsBuilder builder) {
@@ -85,5 +109,8 @@ public class BroDataCommand {
         final Player playerTarget = selector.resolve(ctx.getSource()).getFirst();
         return this.manager.getPlayer(playerTarget).orElseThrow();
     }
-    
+
+    public LiteralCommandNode<CommandSourceStack> getCommand() {
+        return command;
+    }
 }
